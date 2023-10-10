c225222b992fb0a3c53bfb677bc0190d1dcb1aeb
Abdullah-167 <abdullahpadhana503@gmail.com>
2023-10-10 23:44:27 +0500

    commit

---
+++ src/Components/Views/Collection/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Section from './Section'
+import Layout from '@/Components/Common/Layout'
+
+const Collection = () => {
+    return (
+        <Layout>
+            <div className='pr-[50px]'>
+                <Section />
+            </div>
+        </Layout>
+    )
+}
+
+export default Collection
---
+++ src/pages/collection.tsx
@@ -0,0 +1,12 @@
+import Collection from '@/Components/Views/Collection'
+import React from 'react'
+
+const collection = () => {
+    return (
+        <main>
+            <Collection />
+        </main>
+    )
+}
+
+export default collection
