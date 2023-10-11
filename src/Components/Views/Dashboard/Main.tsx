f60342a0291e6bb8f4089a344315e57b01612367
Abdullah-167 <abdullahpadhana503@gmail.com>
2023-10-11 20:44:10 +0500

    commit

--- src/Components/Views/Dashboard/Main.tsx
+++ src/Components/Views/Dashboard/Main.tsx
@@ -6,7 +6,27 @@ const Main = () => {
         <section>
             <div className='max-w-[1200px]'>
                 <h1 className=' text-[#111827] font-bold text-2xl pt-[25px] pb-[31px]'>Dashboard</h1>
-                <textarea className=' bg-[#F3F4F6] max-h-[250px] min-h-[250px] w-full outline-none max-w-[1168px] px-5 py-5 mb-[60px]'></textarea>
+                <div className=' bg-[#F3F4F6] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-[10px]  min-h-[250px] w-full outline-none max-w-[1168px] px-5 py-5 mb-[60px]'>
+                    {textData.map((item, index) => {
+                        return (
+                            <div className='bg-[white] w-full pt-[18px] pb-[15px] pr-[14px] pl-5 border border-[#E5E7EB] sm:max-w-[274px]'
+                                key={index}
+                            >
+                                <h1 className=' text-[#111827] text-base font-bold pb-[7px]'>{item.heading}</h1>
+                                <p className='text-[#797F88] text-sm pb-[10px]'>{item.text}</p>
+                                <div className='flex gap-1 justify-end items-center text-[#797F88] text-xs'>
+                                    <p>{item.btn}</p>
+                                    <span>
+                                        <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M3.4375 1.875L6.5625 5L3.4375 8.125" stroke="#797F88" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                        </svg>
+                                    </span>
+                                </div>
+                            </div>
+                        )
+                    })}
+
+                </div>
                 <div className=' grid grid-cols-1 sm:grid-cols-2 gap-x-[48px] w-full max-w-[1168px]'>
                     {data.map((item, index) => {
                         return (
@@ -52,5 +72,50 @@ const data = [
         heading: 'Hilfe',
         para: 'Das Hilfe Zentrum rund um die Funktionen von amenda.',
         img: '/icon1.svg'
+    },
+]
+
+
+
+const textData = [
+    {
+        heading: '170 m²',
+        text: 'Brutto-Grundfläche (BGF)',
+        btn: 'Summe'
+    },
+    {
+        heading: '170 m²',
+        text: 'Brutto-Grundfläche (BGF)',
+        btn: 'Summe'
+    },
+    {
+        heading: '170 m²',
+        text: 'Brutto-Grundfläche (BGF)',
+        btn: 'Summe'
+    },
+    {
+        heading: '170 m²',
+        text: 'Brutto-Grundfläche (BGF)',
+        btn: 'Summe'
+    },
+    {
+        heading: '170 m²',
+        text: 'Brutto-Grundfläche (BGF)',
+        btn: 'Summe'
+    },
+    {
+        heading: '170 m²',
+        text: 'Brutto-Grundfläche (BGF)',
+        btn: 'Summe'
+    },
+    {
+        heading: '170 m²',
+        text: 'Brutto-Grundfläche (BGF)',
+        btn: 'Summe'
+    },
+    {
+        heading: '170 m²',
+        text: 'Brutto-Grundfläche (BGF)',
+        btn: 'Summe'
     },
 ]
