941b1753f7cded4b38b1923dabe4892c350fef69
Abdullah-167 <abdullahpadhana503@gmail.com>
2023-10-13 18:00:50 +0500

    commit done

---
+++ src/pages/media.tsx
@@ -0,0 +1,12 @@
+import Media from '@/Components/Views/Media'
+import React from 'react'
+
+const media = () => {
+    return (
+        <main>
+            <Media />
+        </main>
+    )
+}
+
+export default media
